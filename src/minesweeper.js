76db0e32db8b3c3b087eb99cdc6a1638c581d8fc
luko <luko.web@gmail.com>
2018-11-03 19:00:28 -0300

    feat: Complete 2nd part of 'Minesweeper'

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,12 +1,22 @@
-const blankLine = '  |   |  ';
-console.log('This is what an empty board would look like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
+// Creating a Game Board (part 2)
 
-const guessLine = '1 |   |  ';
-const bombLine = '  | B |  ';
-console.log('This is what a board with a guess and a bomb on it would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+const board = [
+  [' ', ' ', ' '], // 0
+  [' ', ' ', ' '], // 1
+  [' ', ' ', ' ']  // 2
+];
+
+
+// arrow function
+const printBoard = board => {
+	console.log('Current Board:');
+	console.log(board[0].join(' | '));
+	console.log(board[1].join(' | '));
+	console.log(board[2].join(' | '));
+};
+
+console.log(printBoard(board));
+
+board[0][1] = '1';
+board[2][2] = 'B';
+console.log(printBoard(board));
