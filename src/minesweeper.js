3ad545f32cc6c2c7f564715b99d82bd91422a09d
luko <luko.web@gmail.com>
2018-11-05 16:07:46 -0300

    fix: Edit minesweeper.js

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,22 +1,22 @@
 // Creating a Game Board (part 2)
 
-const board = [
-  [' ', ' ', ' '], // 0
-  [' ', ' ', ' '], // 1
-  [' ', ' ', ' ']  // 2
+const exampleBoard = [
+  [' ', ' ', ' '], // first row
+  [' ', ' ', ' '], // second row
+  [' ', ' ', ' ']  // third row
 ];
 
 
 // arrow function
 const printBoard = board => {
 	console.log('Current Board:');
-	console.log(board[0].join(' | '));
+	console.log(board[0].join(' | ')); // join array method
 	console.log(board[1].join(' | '));
 	console.log(board[2].join(' | '));
 };
 
-console.log(printBoard(board));
+console.log(printBoard(exampleBoard));
 
-board[0][1] = '1';
-board[2][2] = 'B';
-console.log(printBoard(board));
+exampleBoard[0][1] = '1';
+exampleBoard[2][2] = 'B';
+console.log(printBoard(exampleBoard));
