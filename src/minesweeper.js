b465d40daf777f8ef7832d045876a4782cb3a828
luko <luko.web@gmail.com>
2018-11-18 15:57:01 -0300

    feat: Add 'minesweeper.js' part 3

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,22 +1,53 @@
-// Creating a Game Board (part 2)
+// Minesweeper (part 3)
 
-const exampleBoard = [
-  [' ', ' ', ' '], // first row
-  [' ', ' ', ' '], // second row
-  [' ', ' ', ' ']  // third row
-];
+// una función para generar el tablero que verá el jugador
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+	const board = [];
+
+	for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+		const row = [];
+		for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+			row.push(' ');
+
+		}
+	board.push(row);
+	}
+	return board;
+};
+
+// una función para generar el tablero que contendrá las bombas (background)
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+	const board = [];
+
+	for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+		const row = [];
+		for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+			row.push(null);
+
+		}
+	board.push(row);
+	}
+
+	let numberOfBombsPlaced = 0;
+	while (numberOfBombsPlaced < numberOfBombs) {
+		const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+		const randomColumnndex = Math.floor(Math.random() * numberOfColumns);
+
+		board[randomRowIndex][randomColumnndex] = 'B';
+
+		numberOfBombsPlaced++;
+	}
+
+	return board;
+};
 
 
 // arrow function
 const printBoard = board => {
-	console.log('Current Board:');
-	console.log(board[0].join(' | ')); // join array method
-	console.log(board[1].join(' | '));
-	console.log(board[2].join(' | '));
+	console.log(board.map(row => row.join(' | ')).join('\n'));
 };
 
-console.log(printBoard(exampleBoard));
+const playerBoard = generatePlayerBoard(3,3);
+const bombBoard = generateBombBoard(3,2,2);
 
-exampleBoard[0][1] = '1';
-exampleBoard[2][2] = 'B';
-console.log(printBoard(exampleBoard));
+printBoard(playerBoard);
